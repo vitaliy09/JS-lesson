36d451b4b480f9d78ebf8abd0421377d9111bf90
Vitaliy <vitaliy.sng@gmail.com>
2018-11-25 18:40:02 +0700

    Домашнее задание

--- dz-1.js
+++ dz-1.js
@@ -1,4 +1,24 @@
-let money = prompt('Сколько вы зарабатываете?', 'OK'),
-    time = prompt('Введите дату рождения в формате YYYY-MM-DD');
+let money = prompt('Ваш бюджет на месяц?'),
+    time = prompt('Введите дату в формате DD-MM-YYYY');
 
 console.log(money,time);
+
+
+let appData = {
+    budzhet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budzhet / 30);
--- script.js
+++ script.js
@@ -41,3 +41,7 @@ console.log(incr++);
 console.log(decr--);
 
 console.log(5%2);
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
