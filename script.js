1c96e2fc459bf7c868675f3372246ef7b81c7594
Vitaliy <vitaliy.sng@gmail.com>
2018-11-25 18:02:00 +0700

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(leftBorderWidth);
+
+var numder = 5;
+var string = "Hello world!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+console.log(4 / 0);
+console.log('string' * 0);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+/* console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+//alert('Hello world');
+
+let answer = prompt('Есть ли вам 18 лет?', 'Да');
+
+console.log(typeof (null)); */
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
